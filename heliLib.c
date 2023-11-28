9d840a94a1c5d07c07413d8b2defe02f64c9794e
Bryan Moffit <moffit@jlab.org>
2023-11-28 01:45:52 -0500

    Clean up, simplify.
    - remove local readback of registers
    - remove epics structure
    - move reg struct and reg masks to header
    - update settle and stable values
    - add string array for available patterns
    - add double array for reporting delay

--- heliLib.c
+++ heliLib.c
@@ -16,65 +16,8 @@
 #include "jvme.h"
 #include "heliLib.h"
 
-typedef struct
-{
-  /* 0x00 */ volatile uint8_t month; // times out (BERR)
-  /* 0x01 */ volatile uint8_t day;
-  /* 0x02 */ uint8_t _blank0[5];
-
-  /* 0x07 */ volatile uint8_t tsettle;
-  /* 0x08 */ uint8_t _blank1;
-  /* 0x09 */ volatile uint8_t tstable;
-  /* 0x0a */ uint8_t _blank2;
-  /* 0x0b */ volatile uint8_t delay;
-
-  /* 0x0c */ uint8_t _blank3;
-  /* 0x0d */ volatile uint8_t pattern;
-  /* 0x0e */ uint8_t _blank4;
-  /* 0x0f */ volatile uint8_t clock;
-} heliRegs;
-
-/* register masks - from drvHel.c */
-#define HELI_TSETTLE_MASK  0x1f	/* 5 bit register */
-#define HELI_TSTABLE_MASK  0x1f	/* 5 bit register */
-#define HELI_DELAY_MASK    0x0f	/* 4 bit register */
-#define HELI_MONTH_MASK	   0xff
-#define HELI_DAY_MASK	   0xff
-#define HELI_YEAR_MASK     0xff
-#define HELI_PATTERN_MASK  0x07	/* 3 bit register */
-#define HELI_CLOCK_MASK    0xff
-
-/* timing values */
-double fTSettleVals[32] =
-  {
-   10, 20, 30, 40, 50, 60, 70, 80,
-   90, 100, 110, 120, 130, 140, 150, 160,
-   170, 180, 190, 200, 250, 300, 350, 400,
-   450, 500, 550, 600, 700, 800, 900, 1000
-  };
-
-double fTStableVals[32] =
-  {
-   400, 500, 600, 700, 800, 900, 971.65, 1000,
-   1001.65, 1318.90, 1348.90, 1500, 2000, 2500, 3000, 3500,
-   4066.65, 4076.65, 5000, 5500, 6000, 6500, 7000, 8233.35,
-   8243.35, 16567, 16667, 33230, 33330, 50000, 100000, 1000000
-  };
-
-double fClockVals[4] =
-  {
-   30, 120, 240, -1
-  };
-
 /* Macro to check for library / pointer initialization */
-#define CHECKHELI  {							\
-    if(hl.initialized == 0)						\
-      {                                                                 \
-        printf("%s: ERROR: Helcity Generator Library is not initialized \n", \
-               __func__);						\
-        return -1;							\
-      }                                                                 \
-  }
+#define CHECKHELI  { if(hl.initialized == 0) {HELI_ERR("Helicity Generator Library is not initialized\n"); return -1;}}
 
 #define HELI_DBG(format, ...) {if (hl.debug==1) {fprintf(stdout,"%s: DEBUG: ",__func__); fprintf(stdout,format, ## __VA_ARGS__);}}
 #define HELI_ERR(format, ...) {fprintf(stderr,"%s: ERROR: ",__func__); fprintf(stderr,format, ## __VA_ARGS__);}
@@ -89,20 +32,63 @@ typedef struct
 {
   int8_t   initialized;       /* Whether (1) or not (0) the library has been initialized */
   volatile heliRegs *dev;     /* Pointer to device registers */
-  volatile heliRegs local;    /* locally stored register values */
   devaddr_t a24_offset;       /* Offset between VME A24 and Local address space */
   pthread_mutex_t rw_mutex;   /* Local library structure Mutex */
   uint8_t  debug;             /* Whether or not to print debug messages to stdout */
 } heliLibVars;
 
 /* Initialize the local structure */
-static heliLibVars hl = {0, NULL, {0,0,{0,0,0,0,0},0,0,0,0,0,0,0,0}, 0, PTHREAD_MUTEX_INITIALIZER, 0};
+static heliLibVars hl = {0, NULL, 0, PTHREAD_MUTEX_INITIALIZER, 0};
 
 #define HLOCK   if(pthread_mutex_lock(&hl.rw_mutex)<0) perror("pthread_mutex_lock");
 #define HUNLOCK if(pthread_mutex_unlock(&hl.rw_mutex)<0) perror("pthread_mutex_unlock");
 
-#define READHELI(_reg) {hl.local._reg = vmeRead8(&hl.dev->_reg);}
 #define WRITEHELI(_reg,_val) {hl.local._reg = _val; vmeWrite8(&hl.dev->_reg, _val);}
+
+/* Settle Time (usec) */
+double fTSettleVals[32] = {
+  5,  10,  15,  20,  25,  30,  35,  40,
+  45,  50,  60,  70,  80,  90, 100, 110,
+  120, 130, 140, 150, 160, 170, 180, 190,
+  200, 250, 300, 350, 400, 450, 500, 1000
+};
+
+/* Stable Time (usec) */
+double fTStableVals[32] = {
+  240.40, 245.40,  250.40,  255.40,   470.85,  475.85,  480.85,  485.85,
+  490.85, 495.85,  500.85,  505.85,   510.85,  515.85,  900,     971.65,
+  1000,  1001.65, 1318.90, 1348.90,  2000,    3000,    4066.65, 5000,
+  6000,  7000,    8233.35, 8243.35, 16567,   16667,   33230,   33330
+};
+
+/* Mode : LineSync / Free Clock */
+double fClockVals[4] =
+  {
+   30, 120, 240, -1
+  };
+
+/* Reporting Delay */
+double fDelayVals[16] =
+  {
+     0,  1,  2,  4,  8,  16,  24,  32,
+    40, 48, 64, 72, 96, 112, 128, 256
+  };
+
+/* Helicity Pattern */
+char sPatternVals[12][256] =
+  {
+    "Pair",
+    "Quartet",
+    "Octet",
+    "Toggle",
+    "Hexo-Quad",
+    "Octo-Quad",
+    "SPARE [Toggle]",
+    "SPARE [Toggle]",
+    "Thue-Morse-64",
+    "16-Quad",
+    "32-Pair"
+  };
 
 int32_t
 heliInit(uint32_t a24_addr, uint16_t init_flag)
@@ -157,17 +143,12 @@ heliInit(uint32_t a24_addr, uint16_t init_flag)
 int32_t
 heliStatus(int32_t print_regs)
 {
-  int _off = 0;
-#define PREG(_reg) {							\
-    printf("  %10.18s (0x%02lx) = 0x%02x%s",				\
-	   #_reg, (unsigned long)&hl.local._reg - (unsigned long)&hl.local, hl.local._reg, \
-	   ((_off++ % 2) == 0) ? "\t" : "\n");				\
-      }
-  HELI_EPICS ep;
-
+  volatile heliRegs hr;
   CHECKHELI;
 
   HLOCK;
+
+#define READHELI(_reg) {hr._reg = vmeRead8(&hl.dev->_reg);}
 
   READHELI(day);
   READHELI(tsettle);
@@ -181,6 +162,13 @@ heliStatus(int32_t print_regs)
   printf("\n");
   printf("--------------------------------------------------------------------------------\n");
   printf("STATUS for JLab Helicity Generator\n");
+
+  int _off = 0;
+#define PREG(_reg) {							\
+    printf("  %10.18s (0x%02lx) = 0x%02x%s",				\
+	   #_reg, (unsigned long)&hr._reg - (unsigned long)&hr, hr._reg, \
+	   ((_off++ % 2) == 0) ? "\t" : "\n");				\
+  }
   if(print_regs)
     {
       printf("\n");
@@ -200,38 +188,18 @@ heliStatus(int32_t print_regs)
 
   printf(" 0x%06x     ", (uint32_t)((devaddr_t)&hl.dev->month - hl.a24_offset));
 
-  printf("0x%02x      ", hl.local.tsettle & HELI_TSETTLE_MASK);
+  printf("0x%02x      ", hr.tsettle & HELI_TSETTLE_MASK);
 
-  printf("0x%02x      ", hl.local.tstable & HELI_TSTABLE_MASK);
+  printf("0x%02x      ", hr.tstable & HELI_TSTABLE_MASK);
 
-  printf("0x%02x      ", hl.local.delay & HELI_DELAY_MASK);
+  printf("0x%02x      ", hr.delay & HELI_DELAY_MASK);
 
-  printf("0x%02x      ", hl.local.pattern & HELI_PATTERN_MASK);
+  printf("0x%02x      ", hr.pattern & HELI_PATTERN_MASK);
 
-  printf("0x%02x      ", hl.local.clock & HELI_CLOCK_MASK);
+  printf("0x%02x      ", hr.clock & HELI_CLOCK_MASK);
 
   printf("\n");
 
-  ep.iClockReadback = hl.local.clock & HELI_CLOCK_MASK;
-  ep.fTSettleReadbackVal = fTSettleVals[hl.local.tstable & HELI_TSTABLE_MASK];
-  switch(ep.iClockReadback)
-    {
-    case 0:
-    case 1:
-    case 2:
-      ep.fFreqReadback = fClockVals[ep.iClockReadback];
-      ep.fTStableReadbackVal = ((1.0 / ep.fFreqReadback) * 1000000.0) - ep.fTSettleReadbackVal;
-      break;
-
-    default:
-      ep.fTStableReadbackVal = fTStableVals[hl.local.tstable & HELI_TSTABLE_MASK];
-      ep.fFreqReadback = (1.0 / (ep.fTSettleReadbackVal + ep.fTStableReadbackVal)) * 1000000.0;
-
-    }
-
-  printf("           %7.1f      ", ep.fTSettleReadbackVal);
-  printf("%7.1f     ", ep.fTStableReadbackVal);
-  printf("               %7.1f ", ep.fFreqReadback);
 
   printf("\n");
   printf("\n");
@@ -267,8 +235,8 @@ heliGetDebug()
 }
 
 int32_t
-heliConfigure(uint8_t tsettle_set, uint8_t tstable_set, uint8_t delay_set,
-	      uint8_t pattern_set, uint8_t clock_set)
+heliSetRegisters(uint8_t tsettle_set, uint8_t tstable_set, uint8_t delay_set,
+		 uint8_t pattern_set, uint8_t clock_set)
 {
   CHECKHELI;
 
@@ -304,73 +272,11 @@ heliConfigure(uint8_t tsettle_set, uint8_t tstable_set, uint8_t delay_set,
 
 
   HLOCK;
-  WRITEHELI(tsettle, tsettle_set);
-  WRITEHELI(tstable, tstable_set);
-  WRITEHELI(delay, delay_set);
-  WRITEHELI(pattern, pattern_set);
-  WRITEHELI(clock, clock_set);
-  HUNLOCK;
-
-  return 0;
-}
-
-int32_t
-heliGetSettings(uint8_t *tsettle_set, uint8_t *tstable_set, uint8_t *delay_set,
-		uint8_t *pattern_set, uint8_t *clock_set)
-{
-  CHECKHELI;
-
-  HLOCK;
-  READHELI(tsettle);
-  READHELI(tstable);
-  READHELI(delay);
-  READHELI(pattern);
-  READHELI(clock);
-
-  *tsettle_set = hl.local.tsettle;
-  *tstable_set = hl.local.tstable;
-  *delay_set = hl.local.delay;
-  *pattern_set = hl.local.pattern;
-  *clock_set = hl.local.clock;
-  HUNLOCK;
-
-
-  return 0;
-}
-
-int32_t
-heliGetEpicsVars(HELI_EPICS *readback)
-{
-  CHECKHELI;
-
-  HLOCK;
-  READHELI(day);
-  READHELI(tsettle);
-  READHELI(tstable);
-  READHELI(delay);
-  READHELI(pattern);
-  READHELI(clock);
-
-  readback->iTsettleReadback = hl.local.tsettle & HELI_TSETTLE_MASK;
-  readback->fTSettleReadbackVal = fTSettleVals[hl.local.tstable & HELI_TSTABLE_MASK];
-
-  readback->iClockReadback = hl.local.clock & HELI_CLOCK_MASK;
-  switch(readback->iClockReadback)
-    {
-    case 0:
-    case 1:
-    case 2:
-      readback->fFreqReadback = fClockVals[readback->iClockReadback];
-      readback->fTStableReadbackVal =
-	((1.0 / readback->fFreqReadback) * 1000000.0) - readback->fTSettleReadbackVal;
-      break;
-
-    default:
-      readback->fTStableReadbackVal = fTStableVals[hl.local.tstable & HELI_TSTABLE_MASK];
-      readback->fFreqReadback =
-	(1.0 / (readback->fTSettleReadbackVal + readback->fTStableReadbackVal)) * 1000000.0;
-    }
-
+  vmeWrite8(&hl.dev->tsettle, tsettle_set);
+  vmeWrite8(&hl.dev->tstable, tstable_set);
+  vmeWrite8(&hl.dev->delay, delay_set);
+  vmeWrite8(&hl.dev->pattern, pattern_set);
+  vmeWrite8(&hl.dev->clock, clock_set);
   HUNLOCK;
 
   return 0;
--- heliLib.h
+++ heliLib.h
@@ -18,22 +18,35 @@
 
 typedef struct
 {
-  int32_t iTsettleReadback;
-  double  fTSettleReadbackVal;
+  /* 0x00 */ volatile uint8_t month; // times out (BERR)
+  /* 0x01 */ volatile uint8_t day;
+  /* 0x02          */ uint8_t _blank0[5];
 
-  int32_t iTStableReadbackVal;
-  double  fTStableReadbackVal;
+  /* 0x07 */ volatile uint8_t tsettle;
+  /* 0x08          */ uint8_t _blank1;
+  /* 0x09 */ volatile uint8_t tstable;
+  /* 0x0a          */ uint8_t _blank2;
+  /* 0x0b */ volatile uint8_t delay;
 
-  double  fFreqReadback;
-  int32_t iClockReadback;
-} HELI_EPICS;
+  /* 0x0c          */ uint8_t _blank3;
+  /* 0x0d */ volatile uint8_t pattern;
+  /* 0x0e          */ uint8_t _blank4;
+  /* 0x0f */ volatile uint8_t clock;
+} heliRegs;
+
+/* register masks - from drvHel.c */
+#define HELI_TSETTLE_MASK  0x1f	/* 5 bit register */
+#define HELI_TSTABLE_MASK  0x1f	/* 5 bit register */
+#define HELI_DELAY_MASK    0x0f	/* 4 bit register */
+#define HELI_MONTH_MASK	   0xff
+#define HELI_DAY_MASK	   0xff
+#define HELI_YEAR_MASK     0xff
+#define HELI_PATTERN_MASK  0x07	/* 3 bit register */
+#define HELI_CLOCK_MASK    0xff
+
 
 int32_t heliInit(uint32_t a24_addr, uint16_t init_flag);
 int32_t heliStatus(int32_t print_regs);
 
-int32_t heliConfigure(uint8_t tsettle_set, uint8_t tstable_set, uint8_t delay_set,
-		      uint8_t pattern_set, uint8_t clock_set);
-int32_t heliGetSettings(uint8_t *tsettle_set, uint8_t *tstable_set, uint8_t *delay_set,
-			uint8_t *pattern_set, uint8_t *clock_set);
-
-int32_t heliGetEpicsVars(HELI_EPICS *readback);
+int32_t heliSetRegisters(uint8_t tsettle_set, uint8_t tstable_set, uint8_t delay_set,
+			 uint8_t pattern_set, uint8_t clock_set);
